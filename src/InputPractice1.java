1518948b3df5cf16e5b664482772649240ec8fad
HarrisonVelascoLHS <100148556@lubbockisd.net>
2023-09-14 09:46:26 -0500

    Purpose

--- src/InputPractice1.java
+++ src/InputPractice1.java
@@ -1,5 +1,38 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//NAME - Harrison Velasco
+//DATE - 9/14/23
+//PURPOSE - Learn how to define, input, and print some variables and their values.
+
+import java.util.Scanner;
+public class InputPractice1 {
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+
+        int intOne, intTwo;
+        double doubleOne;
+        boolean boolOne;
+
+
+        System.out.print("Enter an integer :: ");
+        intOne = keyboard.nextInt();
+        System.out.print("Enter an integer :: ");
+        intTwo = keyboard.nextInt();
+        System.out.print("Enter a float :: ");
+        doubleOne = keyboard.nextFloat();
+        System.out.print("Enter a bool, (true or false) :: ");
+        boolOne = keyboard.nextBoolean();
+
+
+
+        System.out.println();
+        System.out.println("integer one = " + intOne );
+        System.out.println("integer two = " + intTwo );
+        System.out.println("float one = " + doubleOne);
+        System.out.println("boolean one = " + boolOne);
+
+        //add output for all variables
     }
+
 }
